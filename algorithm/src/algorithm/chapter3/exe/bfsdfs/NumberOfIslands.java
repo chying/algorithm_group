1df80f5ee93e68529c8c569095ca09523ab436ab
chying <852342406@qq.com>
2019-12-16 13:12:56 +0800

    island

--- algorithm/src/algorithm/chapter3/exe/bfsdfs/NumberOfIslands.java
+++ algorithm/src/algorithm/chapter3/exe/bfsdfs/NumberOfIslands.java
@@ -12,14 +12,71 @@ package algorithm.chapter3.exe.bfsdfs;
  */
 public class NumberOfIslands {
 	public int numIslands(char[][] grid) {
-		return 0;
+		int result = 0;
+		if (null != grid && grid.length > 0) {
+			int m = grid.length;
+			int n = grid[0].length;
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (grid[i][j] == '1') {
+						result++;
+						drillDown(i, j, m, n, grid);
+					}
+				}
+			}
+		}
+		return result;
+	}
+
+	private void drillDown(int i, int j, int m, int n, char[][] grid) {
+		if (i < 0 || i >= m || j < 0 || j >= n) {
+			return;
+		}
+		if (grid[i][j] == '1') {
+			grid[i][j] = '0';
+		} else {
+			return;
+		}
+		drillDown(i + 1, j, m, n, grid);
+		drillDown(i - 1, j, m, n, grid);
+		drillDown(i, j + 1, m, n, grid);
+		drillDown(i, j - 1, m, n, grid);
+
 	}
 
 	public static void main(String[] args) {
 		NumberOfIslands a = new NumberOfIslands();
 		char[][] grid = new char[][] { { '1', '1', '1', '1', '0' }, { '1', '1', '0', '1', '0' },
 				{ '1', '1', '0', '0', '0' }, { '0', '0', '0', '0', '0' } };
-				
+
 		System.out.println(a.numIslands(grid));
+	}
+
+	private int n;
+	private int m;
+
+	public int numIslands1(char[][] grid) {
+		int count = 0;
+		n = grid.length;
+		if (n == 0)
+			return 0;
+		m = grid[0].length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++)
+				if (grid[i][j] == '1') {
+					DFSMarking(grid, i, j);
+					++count;
+				}
+		}
+		return count;
+	}
+
+	private void DFSMarking(char[][] grid, int i, int j) {
+		if (i < 0 || i == grid.length || j < 0 || j == grid[i].length || grid[i][j] == '0')
+			return;
+		grid[i][j] = '0';
+		int[] d = { 0, 1, 0, -1, 0 };
+		for (int k = 0; k < 4; k++)
+			DFSMarking(grid, i + d[k], j + d[k + 1]);
 	}
 }
