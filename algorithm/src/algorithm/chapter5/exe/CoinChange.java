d8b1327678be7a7258f6e5b69505041df9db33a2
chying <852342406@qq.com>
2019-12-21 14:43:39 +0800

    dp

--- algorithm/src/algorithm/chapter5/exe/CoinChange.java
+++ algorithm/src/algorithm/chapter5/exe/CoinChange.java
@@ -1,6 +1,10 @@
 package algorithm.chapter5.exe;
 
-
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 【322. 零钱兑换】给定不同面额的硬币 coins 和一个总金额
@@ -15,14 +19,60 @@ package algorithm.chapter5.exe;
 public class CoinChange {
 	public int coinChange(int[] coins, int amount) {
 		if (null != coins) {
-
+			int[] dp = new int[amount + 1];
+			Arrays.fill(dp, amount + 1);
+			dp[0] = 0;
+			for (int i = 1; i <= amount; i++) {
+				for (int coin : coins) {
+					if (i - coin >= 0) {
+						dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+					}
+				}
+			}
+			return dp[amount] < amount + 1 ? dp[amount] : -1;
 		}
-		return 0;
+		return -1;
 	}
 
 	public static void main(String[] args) {
 		CoinChange a = new CoinChange();
 		int[] coins = new int[] { 1, 2, 5 };
 		System.out.println(a.coinChange(coins, 11));
+	}
+
+	/**
+	 * 超出内存限制
+	 * 
+	 * @param coins
+	 * @param amount
+	 * @return
+	 */
+	public int coinChange_BFS(int[] coins, int amount) {
+		if (amount == 0) {
+			return 0;
+		}
+		if (null != coins) {
+			Deque<Integer> deque = new ArrayDeque<Integer>();
+			deque.addFirst(amount);
+			Set<Integer> visitedSet = new HashSet<>();
+			int res = 0;
+			while (!deque.isEmpty()) {
+				int size = deque.size();
+				res++;
+				for (int i = 0; i < size; i++) {
+					int val = deque.pollLast();
+					for (int coin : coins) {
+						int value = val - coin;
+						if (value == 0) {
+							return res;
+						} else if (value > 0 && !visitedSet.contains(value)) {
+							visitedSet.add(value);
+							deque.addFirst(value);
+						}
+					}
+				}
+			}
+		}
+		return 0;
 	}
 }
--- algorithm/src/algorithm/chapter5/template/MaximumProductSubarray.java
+++ algorithm/src/algorithm/chapter5/template/MaximumProductSubarray.java
@@ -8,14 +8,46 @@ package algorithm.chapter5.template;
  */
 public class MaximumProductSubarray {
 	public int maxProduct(int[] nums) {
-		
+		if (null != nums && nums.length > 0) {
+			int max = nums[0];
+			int min = nums[0];
+			int result = nums[0];
+			for (int i = 1; i < nums.length; i++) {
+				if (nums[i] < 0) {
+					int temp = max;
+					max = min;
+					min = temp;
+				}
+				max = Math.max(max*nums[i], nums[i]);
+				min = Math.min(min*nums[i], nums[i]);
+				result = Math.max(max, result);
+			}
+			return result;
+		}
 		return 0;
 	}
 
 	public static void main(String[] args) {
 		MaximumProductSubarray a = new MaximumProductSubarray();
-		int[] nums = new int[] { 2, 3, -2, 4 };
+		int[] nums = new int[] { -4, -3 };
 
 		System.out.println(a.maxProduct(nums));
+	}
+
+	public int maxProduct1(int[] nums) {
+		int max = 1;
+		int min = 1;
+		int re_max = Integer.MIN_VALUE;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] < 0) {
+				int temp = max;
+				max = min;
+				min = temp;
+			}
+			max = Math.max(nums[i] * max, nums[i]);
+			min = Math.min(nums[i] * min, nums[i]);
+			re_max = Math.max(max, re_max);
+		}
+		return re_max;
 	}
 }
